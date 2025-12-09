c1dc4098ba214d5c457c923bf0a06e675b105cd6
yunzhou <os72819371@gmail.com>
2025-12-09 13:12:27 +0800

    feat:调整跨域策略

--- pkg/gin/middleware/cors.go
+++ pkg/gin/middleware/cors.go
@@ -10,11 +10,20 @@ import (
 func Cors() gin.HandlerFunc {
 	return cors.New(
 		cors.Config{
-			AllowOrigins:     []string{"*"},
-			AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"},
-			AllowHeaders:     []string{"Origin", "Authorization", "Content-Type", "Accept"},
-			ExposeHeaders:    []string{"Content-Length", "text/plain", "Authorization", "Content-Type"},
-			AllowCredentials: true,
+			AllowOrigins: []string{"*"},
+			AllowMethods: []string{"GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"},
+			AllowHeaders: []string{
+				"Origin", "Authorization", "Content-Type", "Accept",
+				"X-Device-Id",  // 设备编码(必填)
+				"X-Client",     // 终端标识(必填), 取值范围：h5 android ios pc
+				"X-Token",      // 登录后颁发的token(必填)
+				"X-Version",    // 客户端版本号(必填)
+				"X-Timestamp",  // 发起请求的毫秒时间戳(必填)
+				"X-RetryTimes", // 发起请求的第几次重试，如0、1、2、3
+				"X-Timeout",    // 连接+读写超时总和，秒单位
+			},
+			ExposeHeaders:    []string{"Content-Length", "text/plain", "Authorization", "Content-Type", "X-Request-Id"},
+			AllowCredentials: false,
 			MaxAge:           12 * time.Hour,
 		},
 	)
