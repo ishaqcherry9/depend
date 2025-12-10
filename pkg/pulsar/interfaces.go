0fb5a4826d4f3f9c72180cb233b622b0297e12b9
robert <salonsakalouan@gmail.com>
2025-12-10 13:31:40 +0900

    feat:可指定consumer worker数目

--- pkg/pulsar/interfaces.go
+++ pkg/pulsar/interfaces.go
@@ -21,36 +21,27 @@ type BatchResult struct {
 
 type Client interface {
 	Producer(opts pulsar.ProducerOptions) (Producer, error)
-
 	Consumer(opts pulsar.ConsumerOptions) (Consumer, error)
-
 	MultiTopicConsumer(opts pulsar.ConsumerOptions) (Consumer, error)
-
 	Reader(opts pulsar.ReaderOptions) (Reader, error)
-
 	BeginTransaction(timeout time.Duration) (pulsar.Transaction, error)
-
 	Close() error
 }
 
 type Producer interface {
 	Send(ctx context.Context, payload []byte) error
 	SendWithKey(ctx context.Context, key string, payload []byte) error
-
 	SendDelayed(ctx context.Context, payload []byte, delay time.Duration) error
-
 	SendBatch(ctx context.Context, messages []BusinessMessage) (*BatchResult, error)
-
 	SendAsync(ctx context.Context, msg *pulsar.ProducerMessage, callback func(id MessageID, msg *pulsar.ProducerMessage, err error))
-
 	SendWithTransaction(ctx context.Context, txn pulsar.Transaction, payload []byte) error
-
 	FlushWithCtx(ctx context.Context) error
-
 	Close() error
 }
 
 type Consumer interface {
+	GetWorkerSize() int
+	SetWorkerSize(size int)
 	Receive(ctx context.Context) (*Message, error)
 	Consume(ctx context.Context, handleFn HandleMessageFn) error
 	Ack(msg *Message) error
