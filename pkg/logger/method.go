85969b2f0288b94691601e540636cc0f24c42ad0
yunzhou <os72819371@gmail.com>
2026-03-16 20:51:41 +0800

    feat:取消日志对middleware的依赖

--- pkg/logger/method.go
+++ pkg/logger/method.go
@@ -5,37 +5,39 @@ import (
 	"fmt"
 	"strings"
 
-	middleware "github.com/ishaqcherry9/depend/pkg/gin/middleware"
 	"go.uber.org/zap"
 )
 
+// ContextRequestIDKey 请求ID的key
+var ContextRequestIDKey = "request_id"
+
 func Debug(c context.Context, msg string, fields ...Field) {
-	fields = append(fields, zap.String(middleware.ContextRequestIDKey, middleware.CtxRequestID(c)))
+	fields = append(fields, AddCommonFields(c)...)
 	getLogger().Debug(msg, fields...)
 }
 
 func Info(c context.Context, msg string, fields ...Field) {
-	fields = append(fields, zap.String(middleware.ContextRequestIDKey, middleware.CtxRequestID(c)))
+	fields = append(fields, AddCommonFields(c)...)
 	getLogger().Info(msg, fields...)
 }
 
 func Warn(c context.Context, msg string, fields ...Field) {
-	fields = append(fields, zap.String(middleware.ContextRequestIDKey, middleware.CtxRequestID(c)))
+	fields = append(fields, AddCommonFields(c)...)
 	getLogger().Warn(msg, fields...)
 }
 
 func Error(c context.Context, msg string, fields ...Field) {
-	fields = append(fields, zap.String(middleware.ContextRequestIDKey, middleware.CtxRequestID(c)))
+	fields = append(fields, AddCommonFields(c)...)
 	getLogger().Error(msg, fields...)
 }
 
 func Panic(c context.Context, msg string, fields ...Field) {
-	fields = append(fields, zap.String(middleware.ContextRequestIDKey, middleware.CtxRequestID(c)))
+	fields = append(fields, AddCommonFields(c)...)
 	getLogger().Panic(msg, fields...)
 }
 
 func Fatal(c context.Context, msg string, fields ...Field) {
-	fields = append(fields, zap.String(middleware.ContextRequestIDKey, middleware.CtxRequestID(c)))
+	fields = append(fields, AddCommonFields(c)...)
 	getLogger().Fatal(msg, fields...)
 }
 
@@ -46,8 +48,8 @@ func Debugf(c context.Context, format string, a ...interface{}) {
 
 // getMessage format with Sprint, Sprintf, or neither.
 func getMessage(c context.Context, template string, fmtArgs ...interface{}) (string, []Field) {
-	fields := make([]Field, 0, 1)
-	fields = append(fields, zap.String(middleware.ContextRequestIDKey, middleware.CtxRequestID(c)))
+	fields := make([]Field, 0)
+	// fields = append(fields, AddCommonFields(c)...)
 	if len(fmtArgs) == 0 {
 		return template, fields
 	}
@@ -66,23 +68,22 @@ func getMessage(c context.Context, template string, fmtArgs ...interface{}) (str
 
 func Infof(c context.Context, format string, a ...interface{}) {
 	msg, fields := getMessage(c, format, a...)
-	getLogger().Info(msg, fields...)
+	Info(c, msg, fields...)
 }
 
 func Warnf(c context.Context, format string, a ...interface{}) {
 	msg, fields := getMessage(c, format, a...)
-	getSugaredLogger().Warnf(format, a...)
-	getLogger().Warn(msg, fields...)
+	Warn(c, msg, fields...)
 }
 
 func Errorf(c context.Context, format string, a ...interface{}) {
 	msg, fields := getMessage(c, format, a...)
-	getLogger().Error(msg, fields...)
+	Error(c, msg, fields...)
 }
 
 func Fatalf(c context.Context, format string, a ...interface{}) {
 	msg, fields := getMessage(c, format, a...)
-	getLogger().Fatal(msg, fields...)
+	Fatal(c, msg, fields...)
 }
 
 func Sync() error {
@@ -94,6 +95,30 @@ func Sync() error {
 	return nil
 }
 
+// 添加公共字段到日志中
+func AddCommonFields(ctx context.Context) []Field {
+	fields := make([]Field, 0, 1)
+	// 请求ID
+	fields = append(fields, zap.String(ContextRequestIDKey, CtxRequestID(ctx)))
+	return fields
+}
+
 func WithFields(fields ...Field) *zap.Logger {
 	return GetWithSkip(0).With(fields...)
+}
+
+func CtxRequestID(ctx context.Context) string {
+	if ctx == nil {
+		return ""
+	}
+	// // gin框架context
+	// if c, ok := ctx.(*gin.Context); ok {
+	// 	return GCtxRequestID(c)
+	// }
+	// 原生context,兼容gin框架context
+	v := ctx.Value(ContextRequestIDKey)
+	if str, ok := v.(string); ok {
+		return str
+	}
+	return ""
 }
